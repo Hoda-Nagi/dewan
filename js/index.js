9fc7166e12ab652d0c75e0a9dde5d50a3b142875
dodonagi <48260197+dodonagi@users.noreply.github.com>
2019-03-30 16:24:42 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+$(".year").click(function() {
+
+	// $(".list").fadeToggle();
+	$(".list").css("opacity","1");
+	
+
+});
+
+
+$(".reset").hover(function() {
+	$(this).css("backgroundColor","#fff");
+
+ });
+
+ $(".reset").mouseleave(function() {
+	$(this).css("backgroundColor","#2B9CB5");
+
+
+ });
+
+});
